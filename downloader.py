6fb5cf3745180c6fb27e83b0610010911dae6de1
clevin95 <carter.levin@yale.edu>
2018-04-25 09:37:24 -0400

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -4,8 +4,8 @@ import botocore
 BUCKET_NAME = 'parking-signs' # replace with your bucket name
 s3 = boto3.resource('s3')
 s3 = boto3.resource('s3',
-         aws_access_key_id="AKIAJACS3JWHBLW5KX7Q",
-         aws_secret_access_key="LW6SH+XflZgV6IIiz9aizphPKQ41TMv+rvTydtuo")
+         aws_access_key_id="",
+         aws_secret_access_key="")
 
 def download_image(image_name):
 	try:
